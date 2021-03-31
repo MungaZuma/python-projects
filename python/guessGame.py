9f83b6722747f3f77dda003074dbbcdeb7ae1ae8
munga zuma <mungazuma78@gmail.com>
2021-03-31 13:39:49 +0300

    commit

--- python/guessGame.py
+++ python/guessGame.py
@@ -14,4 +14,26 @@ def gesi(x):
 
     print(f"Hongera umechagua namba sahihi: {randomNo}")
 
-gesi(20)
+# gesi(20)
+
+def compGesi(z):
+    chini = 1
+    juu = z
+    jibu =""
+
+    while jibu != "s":
+        if chini != juu:
+            randno = random.randint(chini,juu)
+        else:
+            randno = chini
+        
+        jibu = input(f"je {randno} iko juu(j), chini(C) au sahihi(s): ")
+
+        if jibu == "j":
+            juu = randno - 1
+        elif jibu == "c":
+            chini = randno + 1
+
+    print(f"hongera bwana computer, {randno} , ndio jibu sahihi")
+
+compGesi(50)
