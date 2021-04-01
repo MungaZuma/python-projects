04fac8f8848ae388c900cfdb563ef9a6f1a264cc
munga zuma <mungazuma78@gmail.com>
2021-04-01 12:34:32 +0300

    second

--- python/projects/rockpapersciscors.py
+++ python/projects/rockpapersciscors.py
@@ -1,20 +1,22 @@
 import random
 
 def play():
-    player = input("what do you select? rock(r), paper(p) or sciscors(s): ")
-
-        # if (player != "r") or (player != "p") or (player != "s"):
-        #     print("please select a valid choice")
     
+    player =""
     computer = random.choice(['r','p','s'])
 
-    if player == computer:
-        return "it's a tie"
-
-    if is_win(player, computer):
-        return "you win"
-
-    return "you lost"
+    
+    while (player != "r") or (player != "p") or (player != "s"):
+        player = input("what do you select? rock(r), paper(p) or sciscors(s): ")
+        print("please select a valid choice")
+        
+        
+        if player == computer:
+            return "it's a tie"
+        if is_win(player, computer):
+            return "you win"
+                
+        return "you lost"
 
 def is_win(player1, cpu):
     if (player1 =="r" and cpu == "s") or (player1 =="s" and cpu == "p") or (player1 =="p" and cpu == "r"):
