a24e0d5875319647958e53250190b347fdd0bef9
munga zuma <mungazuma78@gmail.com>
2021-04-01 12:10:10 +0300

    what is left is to make sure that i can limit the choice and also make it contnious play

---
+++ python/projects/rockpapersciscors.py
@@ -0,0 +1,24 @@
+import random
+
+def play():
+    player = input("what do you select? rock(r), paper(p) or sciscors(s): ")
+
+        # if (player != "r") or (player != "p") or (player != "s"):
+        #     print("please select a valid choice")
+    
+    computer = random.choice(['r','p','s'])
+
+    if player == computer:
+        return "it's a tie"
+
+    if is_win(player, computer):
+        return "you win"
+
+    return "you lost"
+
+def is_win(player1, cpu):
+    if (player1 =="r" and cpu == "s") or (player1 =="s" and cpu == "p") or (player1 =="p" and cpu == "r"):
+        return True
+
+
+print(play())
